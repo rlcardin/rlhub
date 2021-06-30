231b8bd1459b91603903f566b43a017d7e77b9fc
ricardo <rlcardorocha@hotmail.com>
2021-06-30 01:30:45 -0300

    wasdwasdw

--- rlhub3.lua
+++ rlhub3.lua
@@ -85,7 +85,7 @@ tab:Button("Free Emotes and Animation Packages (R15)", "Animations/Dances GUI", 
     loadstring(game:HttpGet("https://gitlab.com/Tsuniox/lua-stuff/-/raw/master/R15GUI.lua"))()
 end)
 
-local tab = win:Tab("FE Admins", "http://www.roblox.com/asset/?id=6023426915")
+local tab = win:Tab("FE Admins/Others FE", "http://www.roblox.com/asset/?id=6023426915")
 tab:Label("Admins")
 tab:Line()
 
@@ -98,6 +98,117 @@ tab:Button("CMD-X Admin", "CMD-X Admin Admin", function()
     Flux:Notification("CMD-X Admin loaded", "Alright")
     loadstring(game:HttpGet("https://pastebin.com/raw/FJCxAASq"))()
 end)
+
+tab:Label("Others FE")
+tab:Line()
+
+tab:Button("Fake VR (R6 Only)","You get fake vr (Work only R6)", function()
+    Flux:Notification("Fake VR Loaded wait.", "Alright")
+        loadstring(game:HttpGet("https://pastebin.com/raw/yWPpG7mq"))()
+    end)
+    tab:Button("FE Black Hole","Use the E key to control", function()
+    Flux:Notification("Black Hole Loaded", "Alright")
+    local UserInputService = game:GetService("UserInputService")
+            local Mouse = game:GetService("Players").LocalPlayer:GetMouse()
+            local Folder = Instance.new("Folder", game:GetService("Workspace"))
+            local Part = Instance.new("Part", Folder)
+            local Attachment1 = Instance.new("Attachment", Part)
+            Part.Anchored = true
+            Part.CanCollide = false
+            Part.Transparency = 1
+            local Updated = Mouse.Hit + Vector3.new(0, 5, 0)
+            local NetworkAccess = coroutine.create(function()
+                settings().Physics.AllowSleep = false
+                while game:GetService("RunService").RenderStepped:Wait() do
+                    for _, Players in next, game:GetService("Players"):GetPlayers() do
+                        if Players ~= game:GetService("Players").LocalPlayer then
+                            Players.MaximumSimulationRadius = 0 
+                            sethiddenproperty(Players, "SimulationRadius", 0) 
+                        end 
+                    end
+                    game:GetService("Players").LocalPlayer.MaximumSimulationRadius = math.pow(math.huge,math.huge)
+                    setsimulationradius(math.huge) 
+                end 
+            end) 
+            coroutine.resume(NetworkAccess)
+            local function ForcePart(v)
+                if v:IsA("Part") and v.Anchored == false and v.Parent:FindFirstChild("Humanoid") == nil and v.Parent:FindFirstChild("Head") == nil and v.Name ~= "Handle" then
+                    Mouse.TargetFilter = v
+                    for _, x in next, v:GetChildren() do
+                        if x:IsA("BodyAngularVelocity") or x:IsA("BodyForce") or x:IsA("BodyGyro") or x:IsA("BodyPosition") or x:IsA("BodyThrust") or x:IsA("BodyVelocity") or x:IsA("RocketPropulsion") then
+                            x:Destroy()
+                        end
+                    end
+                    if v:FindFirstChild("Attachment") then
+                        v:FindFirstChild("Attachment"):Destroy()
+                    end
+                    if v:FindFirstChild("AlignPosition") then
+                        v:FindFirstChild("AlignPosition"):Destroy()
+                    end
+                    if v:FindFirstChild("Torque") then
+                        v:FindFirstChild("Torque"):Destroy()
+                    end
+                    v.CanCollide = false
+                    local Torque = Instance.new("Torque", v)
+                    Torque.Torque = Vector3.new(100000, 100000, 100000)
+                    local AlignPosition = Instance.new("AlignPosition", v)
+                    local Attachment2 = Instance.new("Attachment", v)
+                    Torque.Attachment0 = Attachment2
+                    AlignPosition.MaxForce = 9999999999999999
+                    AlignPosition.MaxVelocity = math.huge
+                    AlignPosition.Responsiveness = 200
+                    AlignPosition.Attachment0 = Attachment2 
+                    AlignPosition.Attachment1 = Attachment1
+                end
+            end
+            for _, v in next, game:GetService("Workspace"):GetDescendants() do
+                ForcePart(v)
+            end
+            game:GetService("Workspace").DescendantAdded:Connect(function(v)
+                ForcePart(v)
+            end)
+            UserInputService.InputBegan:Connect(function(Key, Chat)
+                if Key.KeyCode == Enum.KeyCode.E and not Chat then
+                   Updated = Mouse.Hit + Vector3.new(0, 5, 0)
+                end
+            end)
+            spawn(function()
+                while game:GetService("RunService").RenderStepped:Wait() do
+                    Attachment1.WorldCFrame = Updated
+                end
+            end)
+              
+        end)
+    
+    tab:Button("FE Kill/Fling","Kill/Fling Gui", function()
+    Flux:Notification("Kill/Fling Loaded", "Alright")
+        loadstring(game:HttpGet("https://pastebin.com/raw/Gqfp5kmb"))()
+    end)
+
+    tab:Button("Ball Spin","You enter an invisible ball", function()
+        Flux:Notification("invisible ball Loaded", "Alright")
+        loadstring(game:HttpGet("https://pastebin.com/raw/XxUcV0fg"))()
+    end)
+    
+    tab:Button("Carry People","Need an item in your hand", function()
+        Flux:Notification("Carry People Loaded", "Alright")
+        loadstring(game:HttpGet("https://pastebin.com/raw/Bc98Turg"))()
+    end)
+    
+    tab:Button("FE Car (R6/Need Avatar item)","Need the Eltoria RX750 item for the script to work", function()
+        Flux:Notification("FE Car GUI loaded", "Alright")
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/MonkoTubeYT/carscript/master/!carscript.lua',true))()
+    end)
+    
+    tab:Button("Gravitation Gun","Hold things with this item", function()
+        Flux:Notification("Gravitation Gun Loaded", "Alright")
+        loadstring(game:HttpGet("https://pastebin.com/raw/TCAqcbtW"))()
+    end)
+    
+    tab:Button("Wall Walk","Walk the walls / Press Z to Activate or Deactivate", function()
+        Flux:Notification("Wall Walk loaded", "Alright")
+        loadstring(game:HttpGet("https://pastebin.com/raw/h3guEeKk"))()
+    end)
 
 local tab = win:Tab("Others Script", "http://www.roblox.com/asset/?id=6023426915")
 tab:Label("Others")
