19a381ca4a23d59ef46821bf74ab0f7afa96b84e
ricardo <rlcardorocha@hotmail.com>
2021-07-05 02:45:35 -0300

    asdawdsd

--- rlHub.lua
+++ rlHub.lua
@@ -206,10 +206,25 @@ local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHept
         game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId, LocalPlayer)
     end)
     oth:NewButton("CTRL + Click TP", "Press CTRL and Click", function()
-        loadstring(game:HttpGet("https://pastebin.com/raw/kF3vFUmh"))()
+        local Plr = game:GetService("Players").LocalPlayer
+        local Mouse = Plr:GetMouse()
+        
+        Mouse.Button1Down:connect(function()
+        if not game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.LeftControl) then return end
+        if not Mouse.Target then return end
+        Plr.Character:MoveTo(Mouse.Hit.p)
+        end)
     end)
-    oth:NewButton("CTRL + Click Destroy", "Press CTRL and Click", function()
-        loadstring(game:HttpGet("https://pastebin.com/raw/1xVgydWC"))()
+
+    oth:NewButton("ALT + Click Destroy", "Press ALT and Click", function()
+        local Plr = game:GetService("Players").LocalPlayer
+        local Mouse = Plr:GetMouse()
+        
+        Mouse.Button1Down:connect(function()
+        if not game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.LeftAlt) then return end
+        if not Mouse.Target then return end
+        Mouse.Target:Destroy()
+        end)
     end)
     
     
